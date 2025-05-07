4bce5cc14a69615441dfb5c5a58e2cbc965e3d0a
NachoXx25 <ignacio.alfonso2002@gmail.com>
2025-05-07 09:42:58 -0400

    build: add necesary dependencies

--- obj/Debug/net9.0/CrudProducts.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrudProducts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudProducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe8d18130194a7a5341f49f13807bd49451bc0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudProducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudProducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
